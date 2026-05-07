6977e5ffa284634ce71cd3210317602e8aaff327
phantomomm <ommsaiiitr123@gmail.com>
2026-05-07 06:08:02 +0000

    variables and datatypes

---
+++ 01_basics/01_basics_variables.js
@@ -0,0 +1,11 @@
+const accountId = 18978
+let accountEmail = "ommflipkart@gmail.com"
+var accountpassword = "87897"
+accountcity = "Bhubaneswar"
+
+accountEmail = "osp@gmail.com"
+accountpassword = "3298091"
+accountcity = "benagaluru"
+
+console.table([accountId, accountEmail, accountpassword, accountcity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict" // using newer version of JS
+
+// alert(3+3) // we are using nodejs and not browser , as the engine of javscript is in a browser
