e7854fc5ccfc53ff6c3ed9813b547f9ef3a39fbf
Omm Sai Prasad <ommsaiiitr123@gmail.com>
2026-05-09 05:47:18 +0000

    Conversion of datatypes

---
+++ 01_basics/03_type_conversion.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let value = Number(score)
+
+//console.log(typeof value)
+//console.log(value)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isloggedin = ""
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+// 1 => true , 0 => false
+// "" => false
