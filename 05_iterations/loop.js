42e1d286c5b47e690dcf2f03ff84d0495972225b
Omm Sai Prasad <ommsaiiitr123@gmail.com>
2026-06-13 20:24:59 +0000

    14/06/26

---
+++ 05_iterations/loop.js
@@ -0,0 +1,17 @@
+// for of
+
+const arr = [1,2,3,4,5]
+
+for(const num of arr){
+   console.log(num);
+}
+
+const greetings = "Hello World!"
+
+for(const greet of greetings){
+     console.log(`each char is ${greet}`)
+}
+
+//Maps
+
+const map =  new Map()
